0b80a96d080f4dba322dfd65b9bec6b58e19bb08
reginoorv <giereg1998@gmail.com>
2026-03-20 12:53:26 +0700

    Isi konfigurasi Supabase

--- js/config.js
+++ js/config.js
@@ -4,5 +4,5 @@
 // Caranya: lihat file PANDUAN-DEPLOY.md
 // =============================================
 
-const SUPABASE_URL  = 'https://XXXXXXXXXXXXXXXX.supabase.co';
-const SUPABASE_ANON = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.XXXXXXXX';
+const SUPABASE_URL  = 'https://tomuiiomostiiobuwhqa.supabase.co';
+const SUPABASE_ANON = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRvbXVpaW9tb3N0aWlvYnV3aHFhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzM5NzcyNDYsImV4cCI6MjA4OTU1MzI0Nn0.uWDMHl_lpQe8YmHmM_P6NRUQPmZ1cdyEbBcaQuRQBhE';
